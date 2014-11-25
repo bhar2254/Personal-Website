ee8ad9ac76e42ce44567c03c307aead1ae380419
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2014-11-25 16:21:14 -0600

    Primary Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Blaine's World</title>
+        <link rel="Stylesheet" type="text/css" href="Stylesheet1.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+<body>
+
+	<div class="main">
+		<div class="sidebar"><li>Lorem</li><li>Ipsum</li><li>Blablabla</li><li>Grammar rules still apply</li></div>
+		<div class="content"><h3>23 Nov 2014</h3>Hello world. How's it been?</div>
+		<div class="clear"></div>
+	</div>
+</body>
+</html>
